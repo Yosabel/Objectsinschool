79e84173a035bc2240b269aadaec47f23dd62998
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2018-01-17 11:35:20 -0800

    hi

---
+++ actions.js
@@ -0,0 +1,9 @@
+var allStudents={};
+function listStudents() {
+var t= "<takeborder='1'>";
+for(var i = 0; i<allstudents.length; i++){
+    t+="<tr><td>" + allStudents[i].firstName+"<td>";
+    }
+}
+t+= "</table>";
+    document.getElementById.innerHTML=t;
