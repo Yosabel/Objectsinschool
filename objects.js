4a5760c9e6a3b0ddef40eea7f5681938957677ea
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2018-01-17 11:22:21 -0800

    Initial commit

---
+++ objects.js
@@ -0,0 +1,43 @@
+function Student(firstName,lastName,grade) {
+    this.id= id++;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.grade = grade;
+
+    /* needs to be implemented
+    this.sections = [];
+     */
+}
+
+function Teacher(firstName,lastName,subject) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.subject = subject;
+
+    /* needs to be implemented
+    this.sections = [];
+     */
+}
+
+function Section(name,count) {
+    this.name = name;
+    this.count = count;
+
+    /* Needs to be implemented
+
+    this.students = [];
+    this.teacher;
+
+    this.addTeacher = function(teacher) {
+        this.teacher = teacher;
+    }
+
+    this.addStudent = function(student) {
+        this.students.push(student);
+    }
+
+    this.removeStudent = function(student) {
+        //remove student
+    }
+    */
+}
