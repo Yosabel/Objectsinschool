c1fe86a8506efdebfd2880a00ac9ad005dddf1a7
yosabel tekeste <yosabeltekeste@students.berkeley.net>
2018-01-22 12:22:04 -0800

    hi

--- actions.js
+++ actions.js
@@ -1,16 +1,53 @@
-function listItems(){
-    var selection = document.getElementById("List").value;
-    // var teacher=allTeachers[selection];
-    // var students=allStudents[selection];
-    // var subject=allSections[selection];
-   var items= totalItems[selection];
-    var result="";
-        for(key in items[i]){
-            items += items[i] +  " ";
-   items+="<br>";
+function listTeachers() {
+var html="<table border>";
+for(var i=0;i<allTeachers.length;i++){
+
+   html+="<td>" + allTeachers[i].firstName + "</td>";
+    html+="<td>" + allTeachers[i].lastName + "</td>";
+    html+="<td>" + allTeachers[i].subject + "</td>";
+    html+="</tr>";
+}
+html+="</table>";
+    document.getElementById("List").value=html;
+}
+
+function listStudents() {
+    var html="<table border>";
+    for(var i=0;i<allStudents.length;i++){
+
+        html+="<td>" + allStudents[i].firstName + "</td>";
+        html+="<td>" + allStudents[i].lastName + "</td>";
+        html+="<td>" + allStudents[i].subject + "</td>";
+        html+="</tr>";
     }
-    document.getElementById("OuptutList").innerHtml = items;
+    html+="</table>";
+    document.getElementById("List").value=html;
 }
 
 
+function listSections() {
+    var html="<table border>";
+    for(var i=0;i<allSections.length;i++){
+
+
+        html+="<td>" + allSections[i].subject + "</td>";
+        html+="</tr>";
+    }
+    html+="</table>";
+    document.getElementById("List").value=html;
+}
+
+function determineList(listType){
+    if(listType===0){
+        listStudents();
+    }
+    console.log(listType);
+    if(listType===1){
+        listTeachers();
+    }
+    if(listType===2){
+        listSections();
+    }
+
+}
 
